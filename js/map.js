fa98269f2732aed7144191e5d8cf49b174710876
Gusbod <skrapiskrap@gmail.com>
2022-06-02 22:44:53 +0200

    added campcluster and coloring from spreadsheet

--- js/map.js
+++ js/map.js
@@ -1,6 +1,10 @@
-var geojsonFiles = ['./data/naturereserve.geojson', './data/fire.geojson', './data/zones.geojson', './data/circles.geojson'];
+var geojsonFiles = ['./data/naturereserve.geojson', './data/fire.geojson', './data/zones.geojson'];
 
 var map = L.map('map', { zoomControl: false }).setView([57.621816, 14.925924], 17);
+
+// map.createPane('labels');
+// map.getPane('labels').style.zIndex = 650; // This pane is above markers but below popups
+// map.getPane('labels').style.pointerEvents = 'none'; // Layers in this pane are non-interactive and do not obscure mouse/touch events
 
 function getStyle(name) 
 {
@@ -19,9 +23,10 @@ function getStyle(name)
             return {
                 "weight": 2,
                 "opacity": 0.75,
-                "color": '#34cceb',
-                // "dashArray": '5',
-                "fillOpacity": 0
+                "color": 'yellow',
+                "dashArray": '5',
+                "fillOpacity": 0,
+                "zIndex": 0
             };
         case 'fire': 
             return {
@@ -30,33 +35,166 @@ function getStyle(name)
                 "color": '#ff3322',
                 "fillOpacity": 0
             };
-        case 'circles':   
+        case 'campclusters':   
             return {
-                "weight": 1,
-                "opacity": 1,
-                "color": '#ffff00',
-                "fillOpacity": 0
+                "weight": 3,
+                "opacity": 0.5,
+                "color": 'lightgrey',
+                "fillOpacity": 0.25,
+                "fillColor": 'grey',
+                "zIndex": 10
+            };
+        case 'campcircles':   
+            return {
+                "weight": 3,
+                "opacity": 0.25,
+                "color": 'yellow',
+                "fillOpacity": 0,
+                "radius": 12.5
             };
         default:           
             return {
-                "fillColor": '#E31A1C',
                 "weight": 1,
                 "opacity": 1,
-                "color": '#ff7800',
-                "dashArray": '5',
                 "fillOpacity": 0
             };
     }
 }
+
+//ADD POINTS AS CIRCLES
+// function createCircles (feature, latlng) 
+// {
+//   return L.circle(latlng)
+// }
+
+// fetch('./data/campcircles.geojson').then(response => response.json()).then(response => 
+//     {
+//         L.geoJson(response, {style: getStyle(response.name), onEachFeature: onEachFeature, pointToLayer: createCircles} )
+//         .addTo(map)
+//         .eachLayer(function (layer) 
+//         {
+// 		    layer.bindPopup(layer.feature.properties.name);
+//             // .bindTooltip(layer.feature.properties.name);
+// 	    });
+//     });
+
+// HIGHLIGHT FEATURES WITH BORDER
+//  --------------------------------
+// let highlightedLayer = null;
+// let highlightedLayerStyle = null;
+
+// function highlightFeature(e) {
+//     var layer = e.target;
+//     highlightedLayer = layer;
+//     highlightedLayerStyle = layer.options.style;
+
+//     layer.setStyle({ weight: 6 });
+// }
+
+//Reset object when deselected
+// map.on('popupclose', function(ev) {
+//     if (highlightedLayer) 
+//     {
+//         highlightedLayer.setStyle(highlightedLayerStyle);
+//         highlightedLayer = null;
+//         highlightedLayerStyle = null;
+//     }
+// });
+
+function onEachFeature(feature, layer) 
+{
+    // layer.on({
+    //     // mouseover: highlightFeature,
+    //     // mouseout: resetHighlight,
+    //     click: highlightFeature
+    // });
+}
+//  --------------------------------
+
+let sheetdata;
+
+fetch('https://sheets.googleapis.com/v4/spreadsheets/1HBERVykwMRDlTGQpOJHmoeDSLVcGriB9V4-FU_F8iag/values/A1:B10?alt=json&key=AIzaSyAwMjpopKPH4_7Kn8qNhrOGz4c-JBv3QG0').then(resp => resp.json()).then(resp => 
+{
+    sheetdata = resp.values;
     
+    fetch('./data/campclusters.geojson').then(response => response.json()).then(response => 
+    {
+        L.geoJson(response, {style: function(feature) 
+        {
+            let color = 'gray';
+
+            //loop thourgh sheetdata
+            for (let i = 0; i < sheetdata.length; i++)
+            {
+                if (sheetdata[i][0] == feature.properties.name)
+                {
+                    if (sheetdata[i][1] > 490) color = 'red';
+                    else if (sheetdata[i][1] > 250) color = 'orange';
+                    else if (sheetdata[i][1] > 1) color = 'green';
+                    else color = 'gray';
+                    break;
+
+                    // return {
+                    //     "color": color,
+                    //     "fillColor": color,
+                    //     "weight": 3,
+                    //     "opacity": 1,
+                    //     "fillOpacity": 0.1
+                    // };
+                }
+            }
+
+            return {
+                "color": color,
+                "fillColor": color,
+                "weight": 3,
+                "opacity": 1,
+                "fillOpacity": 0.1
+            };
+        }, onEachFeature: matchClusterData} )
+        .addTo(map)
+        .eachLayer(function (layer) {layer.bindPopup(layer.feature.properties.name)});
+    });
+
+});
+
+// L.geoJSON(states, {
+//     style: function(feature) {
+//         switch (feature.properties.party) {
+//             case 'Republican': return {color: "#ff0000"};
+//             case 'Democrat':   return {color: "#0000ff"};
+//         }
+//     }
+// }).addTo(map);
+
+function matchClusterData(feature, layer) 
+{
+    // console.log(feature.properties.name);
+}
+
+// fetch('./data/campclusters.geojson').then(response => response.json()).then(response => 
+// {
+//     L.geoJson(response, {style: getStyle(response.name), onEachFeature: matchClusterData} ).addTo(map)
+//     .eachLayer(function (layer) {layer.bindPopup(layer.feature.properties.name)
+//     //  .bindTooltip(layer.feature.properties.name);
+// });
+// });
+
 //loop through geojsonfiles, use fetch to download them and add as a layer to the map
 for (var i = 0; i < geojsonFiles.length; i++) 
 {
     fetch(geojsonFiles[i]).then(response => response.json()).then(response => 
     {
-        L.geoJson(response, {style: getStyle(response.name)} ).addTo(map);
+        L.geoJson(response, {style: getStyle(response.name), onEachFeature: onEachFeature} ).addTo(map)
+        .eachLayer(function (layer) {layer.bindPopup(layer.feature.properties.name)
+        //  .bindTooltip(layer.feature.properties.name);
+	});
     });
 }
+
+
+
+
 
     // var options = { 
     //   corridor: 5,
